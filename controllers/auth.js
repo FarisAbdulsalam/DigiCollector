c8abdc0e74e3c1ad3c64af103ba46511c1f5642e
Faris <farisalmannaei@outlook.com>
2025-06-24 11:42:03 +0300

    done with auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,56 @@ const router = require('express').Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
-router.get('/sign-up', (req, res) => {
+router.get('/sign-up', async (req, res) => {
     res.render('auth/sign-up.ejs');
 });
+
+router.post('/sign-up', async (req, res) => {
+    const username = req.body.username;
+    const usernameExists = await User.findOne({username});
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    if(usernameExists){
+        return res.send('Username already exists');
+    }
+    if(password !== confirmPassword){
+        return res.send('Passwords do not match');
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const userDetails = {
+        username: username,
+        password: hashedPassword
+    };
+    const user = await User.create(userDetails);
+    res.send(`Thanks for signing up ${user.username}`);
+    req.session.user = {
+        username: userExists.username,
+        _id: userExists._id
+    };
+    res.redirect('/');
+});
+
+router.get('/sign-in', (req, res) => {
+    res.render('auth/sign-in.ejs');
+});
+
+router.post('/sign-in', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const userExists = await User.findOne({ username: req.body.username });
+    if(!userExists){
+        res.send('Something went wrong. Try logging in again.');
+    }
+    const isValidPassword = await bcrypt.compare(password, userExists.password);
+    if(!isValidPassword){
+        return res.send('Something went wrong. Try logging in again.')
+    }
+    req.session.user = {
+        username: userExists.username,
+        _id: userExists._id
+    };
+    res.redirect('/');
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,45 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+//middleware
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+const passUserToView = require('./middleware/pass-user-to-view');
+const isSignedIn = require('./middleware/is-signed-in');
+
+//read PORT from .env or default to 3000
+const PORT = process.env.PORT ? process.env.PORT : 3000;
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+});
+
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true
+    })
+)
+app.use(passUserToView);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+const authController = require('./controllers/auth.js');
+app.use('/auth', authController);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
