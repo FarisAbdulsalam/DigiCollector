a8001388ebe6f78eb55485b9f41f375514182467
Faris <farisalmannaei@outlook.com>
2025-06-26 02:20:15 +0300

    added sign out functionality, working on cardCollections controller

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     collection: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: cardCollection
+        ref: 'cardCollection'
     }
 });
 
