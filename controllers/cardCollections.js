d4af2358b056cec6f2c3fdcc42e88549ab2db391
Faris <farisalmannaei@outlook.com>
2025-06-26 03:15:21 +0300

    added create collection functionality

--- controllers/cardCollections.js
+++ controllers/cardCollections.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
 const cardCollection = require('../models/cardCollection.js');
 
-router.get('/mycollection', async (req, res) => {
+router.get('/my-collection', async (req, res) => {
     const owner = req.session.user;
     const collection = await cardCollection.findOne({owner});
-    res.render('cardCollections/mycollection.ejs', { collection });
+    if(!collection){
+        return res.render('cardCollections/new-collection.ejs')
+    }
+    res.render('cardCollections/my-collection.ejs', { collection });
+});
+
+router.post('/', async (req, res) => {
+    const owner = req.session.user;
+    const collectionName = req.body.collectionName;
+    const newCollection = await cardCollection.create({
+        owner: owner,
+        collectionName: collectionName,
+        cards: []
+    });
+    res.redirect('/cardCollections/my-collection');
 });
 
 module.exports = router;
